46f4a82589c9832d5490bb7cac03b2dc255af0df
Alexandr <milan.801813@gmail.com>
2022-12-17 11:58:30 +0300

    2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу,которая принимает на вход
+ //координаты точки (X и Y),причем X ≠ 0 и Y ≠ 0 и выдаёт номер
+ // четверти плоскости, в которой находится эта точка.
+
+int[] points = new int[2];
+
+for (int i = 0; i < points.Length; i++)
+{
+points[i] = Convert.ToInt32(Console.ReadLine());
+}
+//points[0] - x, points[1] - y. [x,y] x > 0, y > 0 = 1
+if (points[0] > 0 && points[1] > 0)
+Console.WriteLine("1");
+if (points[0] < 0 && points[1] > 0)
+Console.WriteLine("2");
+if (points[0] < 0 && points[1] < 0)
+Console.WriteLine("3");
+if (points[0] > 0 && points[1] < 0)
+Console.WriteLine("4");
